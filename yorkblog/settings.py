7eedf72415bfbd5bc0c9673c3d12ca15d655d308
Ryan <evan.ryan.york@gmail.com>
2019-04-06 17:40:52 -0500

    with css

--- yorkblog/settings.py
+++ yorkblog/settings.py
@@ -114,7 +114,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
